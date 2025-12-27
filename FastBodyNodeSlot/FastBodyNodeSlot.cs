da4115be3c19a32aec6375aae5e44fb8966ab146
Raidriar796 <145519075+Raidriar796@users.noreply.github.com>
2025-12-26 22:01:18 -0800

    Setup variables for caching
    This contains the necessary class and dictionary for storing information that will be used to compare for changes.

--- FastBodyNodeSlot/FastBodyNodeSlot.cs
+++ FastBodyNodeSlot/FastBodyNodeSlot.cs
@@ -54,4 +54,18 @@ public partial class FastBodyNodeSlot : ResoniteMod
 	        return false;
         }
     }
+
+    // Instantiated to cache data to compare for changes
+    private class CachedResults(User user, BodyNode node)
+    {
+        public User CachedUser { get; set; } = user;
+        public BodyNode CachedNode { get; set; } = node;
+        public Slot CachedSlot { get; set; } = user.GetBodyNodeSlot(node);
+        public Slot CachedParent { get; set; } = null!;
+        public AvatarObjectSlot CachedAvatarObjectSlot { get; set; } = null!;
+        public IAvatarObject CachedEquippedAvatar { get; set; } = null!;
+    }
+
+    // Stores the instance of the BodyNodeSlot with it's cached results
+    private static readonly Dictionary<BodyNodeSlot, CachedResults> CachedBodyNodeSlots = [];
 }
